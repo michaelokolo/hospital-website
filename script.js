7726e3d08673b96ec5612c0fcc9f5805d5faa517
Michael Okolo <michaelokolo62@yahoo.com>
2022-12-23 18:37:57 -0500

    js to close navbar by clicking anywhere on the document

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
+
+document.onclick = (d) => {
+    if(d.target.id !== 'menu-btn' && d.target.class !== 'navbar'){
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    }
+}
+
+
+
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
